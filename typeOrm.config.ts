65cca14aba17f7a1e2b1666732f8b2ba26174fd1
Yurii Holskyi <yurii.golskyi@gmail.com>
2023-08-23 09:51:55 +0300

    Set up migrations

---
+++ typeOrm.config.ts
@@ -0,0 +1,14 @@
+import { Coffee } from './src/coffees/entities/coffee.entity';
+import { Flavor } from './src/coffees/entities/flavor.entity';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: [Coffee, Flavor],
+  migrations: ['dist/migrations/*.js'],
+});
